8499c77e10a88a83371e5464c77768f4d31555a9
ymraintree <ymraintree@gmail.com>
2011-04-18 17:08:33 +0900

    テスト

--- src/main/scala/org/crashball/Pad.scala
+++ src/main/scala/org/crashball/Pad.scala
@@ -16,8 +16,7 @@ object Pad {
 
 class Pad extends ActiveObject {
 
-//  private var mScreenWide = 100
-  private var mScreenWide = 200
+  private var mScreenWide = 100
   var x = .0f
   var y = .0f
   private var mTouchX: Float = 100
